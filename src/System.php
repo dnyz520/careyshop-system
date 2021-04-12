9666928a692bf39a27412ae274ce066d201009dd
思维空间 <dnyz520@126.com>
2021-04-12 11:33:50 +0800

    up

---
+++ src/System.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace careyshop;
+
+class System
+{
+    /**
+     * 加载二次发开公共函数
+     * @access public static
+     * @return void
+     */
+    public static function loadFunction()
+    {
+    }
+}
---
+++ src/SystemService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace careyshop;
+
+use think\Service;
+
+class SystemService extends Service
+{
+    public function boot()
+    {
+    }
+}
